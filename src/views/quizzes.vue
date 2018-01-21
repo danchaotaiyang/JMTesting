5c339e36145cf3a3793d5a60faba19a4db8d2f65
danchaotaiyang <yuxiaolong00@qq.com>
2018-01-22 00:31:13 +0800

    创建项目

---
+++ src/views/quizzes.vue
@@ -0,0 +1,59 @@
+<template>
+<transition name="quizzes">
+    <div class="quizzes">
+        <div class="introduce"></div>
+        <swiper class="subject" :options="swiperOption">
+            <swiper-slide v-for="(slide, index) in swiperSlides" :key="index">
+                <div class="stuff"></div>
+                <div class="options"></div>
+            </swiper-slide>
+        </swiper>
+    </div>
+</transition>
+</template>
+
+<script>
+import {swiper, swiperSlide} from 'vue-awesome-swiper';
+
+export default {
+    data() {
+        return {
+            swiperOption: {
+                loop : true,
+                autoplay:true
+            },
+            swiperSlides: [1, 2, 3, 4, 5]
+        };
+    },
+    components: {
+        swiper, swiperSlide
+    }
+}
+</script>
+
+<style lang="scss" rel="stylesheet/scss" scoped>
+@import "../assets/sass/util/variables";
+@import "../assets/sass/util/mixins";
+
+.quizzes {
+    width: 100%;
+    height: 100%;
+    overflow: hidden;
+}
+.subject {
+    position: fixed;
+    top: 11vw;
+    bottom: 0;
+    background-color: #ccc;
+}
+
+.quizzes-enter-active,
+.quizzes-leave-active {
+    @include transition(all .15s ease-out);
+}
+
+.quizzes-enter,
+.quizzes-leave-to {
+    @include transform(translate(100%, 0));
+}
+</style>
