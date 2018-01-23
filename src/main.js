bd29fa8730404448d1d87e20b0478dfc8efe92a6
danchaotaiyang <yuxiaolong00@qq.com>
2018-01-23 19:13:10 +0800

    Update

--- src/main.js
+++ src/main.js
@@ -2,6 +2,10 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 
+import Axios from 'axios';
+
+Vue.prototype.$http = Axios;
+
 new Vue({
     router,
     render: h => h(App)
